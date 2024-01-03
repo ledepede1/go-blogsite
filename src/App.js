437ba2ef83036f9b15331db30b16bb2a6c440946
Mikkel <mikkelhulstroem@gmail.com>
2024-01-03 17:02:58 +0100

    Removed unnecesarry stuff cleand a bit up.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import SignupForm from './pages/signup';
+import MainPage from './pages/mainpage';
+import { Navigate } from 'react-router-dom';
 import './App.css';
 
+
+export const PrivateRoute = ({ children}) => {
+    const isAuthenticated = true;
+        
+    if (isAuthenticated ) {
+      return children
+    }
+      
+    return <Navigate to="/login" />
+}
+
 function App() {
+
     return (
         <Router>
             <div className="App">
@@ -12,6 +26,14 @@ function App() {
                     <Routes>
                         <Route path="/login" element={<Login />} />
                         <Route path="/" element={<SignupForm />} />
+                        <Route 
+                        path="/main" 
+                        element={
+                        <PrivateRoute>
+                        <MainPage />
+                        </PrivateRoute>
+                        } 
+                        />
                     </Routes>
                 </div>
             </div>
--- users.sql
+++ users.sql
@@ -1,9 +1,13 @@
-CREATE DATABASE IF NOT EXISTS `users` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `users`;
+CREATE DATABASE IF NOT EXISTS `blogsite`;
+USE `blogsite`;
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `username` varchar(50) DEFAULT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `message` mediumtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(50) DEFAULT NULL,
   `password` varchar(5000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
